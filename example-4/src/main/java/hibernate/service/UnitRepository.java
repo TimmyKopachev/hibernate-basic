549e7c0147e47432d351cb23af36f37b88216caa
Timmy-PC <Warrior0076@yandex.ru>
2022-06-10 02:32:39 +0300

    update SQL @Query

--- example-4/src/main/java/hibernate/service/UnitRepository.java
+++ example-4/src/main/java/hibernate/service/UnitRepository.java
@@ -12,12 +12,13 @@ public interface UnitRepository extends JpaRepository<Unit, Long> {
   @Override
   @Query(value = """
        WITH RECURSIVE units_tree (id, name, parent_id) AS (
-       SELECT id, name, parent_id" + " FROM unit
+       SELECT id, name, parent_id FROM unit
          UNION ALL
        SELECT u.id, u.name, u.parent_id FROM unit u
        INNER JOIN units_tree ut ON ut.id = u.parent_id  )
        SELECT id, name, parent_id
-      FROM units_tree WHERE parent_id IS NULL
+      FROM units_tree
+      WHERE parent_id IS NULL
       """, nativeQuery = true)
   List<Unit> findAll();
 }
