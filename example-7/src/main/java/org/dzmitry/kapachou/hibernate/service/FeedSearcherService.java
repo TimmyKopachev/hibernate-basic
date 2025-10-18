b26d8d0640098012ff631415a8766a74f5808589
Timmy-PC <Warrior0076@yandex.ru>
2025-10-18 21:45:12 +0300

    fix

--- example-7/src/main/java/org/dzmitry/kapachou/hibernate/service/FeedSearcherService.java
+++ example-7/src/main/java/org/dzmitry/kapachou/hibernate/service/FeedSearcherService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.dzmitry.kapachou.hibernate.model.Project;
 import org.dzmitry.kapachou.hibernate.model.RelevantProjectFeed;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -14,14 +15,14 @@ public class FeedSearcherService {
     private final ProjectFeedRepository projectRepository;
     private final ClientRepository clientRepository;
 
+    @Transactional(readOnly = true)
     public Collection<Project> lookupRelevantProjects(Long clientId) {
         return projectRepository.findProjectsByRadius(clientId);
     }
 
+    @Transactional(readOnly = true)
     public RelevantProjectFeed buildFeedForClient(Long clientId) {
         var client = clientRepository.findById(clientId).orElseThrow();
-
-
         Collection<Project> feed = projectRepository.findProjectsByRadius(client.getId());
         return new RelevantProjectFeed(client, feed);
     }
